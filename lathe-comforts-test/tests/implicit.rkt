4d6086f545d98ef786d5d710cdb02203e5891189
Ross Angle <rokitna@hotmail.com>
2020-09-29 16:56:12 -0700

    INCOMPLETE: Write tests for using `let-implicit-equals-transformer-bindings` with two different sets of scopes at once.

--- lathe-comforts-test/tests/implicit.rkt
+++ lathe-comforts-test/tests/implicit.rkt
@@ -20,7 +20,7 @@
 
 
 (require #/for-syntax racket/base)
-(require #/for-syntax #/only-in syntax/parse expr)
+(require #/for-syntax #/only-in syntax/parse expr id)
 
 (require #/for-syntax lathe-comforts/implicit)
 (require #/for-syntax lathe-comforts/maybe)
@@ -36,22 +36,17 @@
 
 ; TODO: Write more unit tests.
 
-; TODO NOW: Figure out some more tests to do on implicit variables. We
-; We should write at least one test where we use
-; `let-implicit-equals-transformer-bindings-with-scopes` to bind at
-; least two implicit variables at the same time with different scope
-; sets.
-
 (define-empty-aux-env)
 
-(check-equal? "correct"
+(check-equal?
   (w- x 1
     (let-implicit-equals-transformer-binding 4 "correct"
       (w- x 2
         (quote-implicit-equals-transformer-part 4))))
+  "correct"
   "Test `let-implicit-equals-transformer-binding` and `quote-implicit-equals-transformer-part` in a simple arrangement")
 
-(check-equal? "correct"
+(check-equal?
   (let-implicit-equals-transformer-binding 4 "cor"
     (let-syntax
       (
@@ -63,6 +58,7 @@
         (string-append
           four
           (quote-implicit-equals-transformer-part 4)))))
+  "correct"
   "Test that a quoted instance of `quote-implicit-equals-transformer-part` in a local macro definition still finds the correct lexically bound implicit variable")
 
 (define-simple-macro
@@ -70,13 +66,14 @@
   (let-implicit-equals-transformer-binding 4 "rect"
     (string-append body (quote-implicit-equals-transformer-part 4))))
 
-(check-equal? "correct"
+(check-equal?
   (let-implicit-equals-transformer-binding 4 "cor"
     (using-implicit-4-as-an-implementation-detail
       (quote-implicit-equals-transformer-part 4)))
+  "correct"
   "Test that a quoted instance of `let-implicit-equals-transformer-binding` in a macro definition doesn't interfere with the caller's implicit variables")
 
-(check-equal? "correct"
+(check-equal?
   (w- x 1
     (let-implicit-equals-transformer-bindings ([4 "cor"] [5 "rect"])
       (w- x 2
@@ -88,6 +85,7 @@
             (string-append
               (quote-implicit-equals-transformer-part 5)
               (quote-implicit-equals-transformer-part 4)))))))
+  "correct"
   "Test using `let-implicit-equals-transformer-bindings` and `local-implicit-equals-transformer-part` to permute bindings")
 
 (check-equal?
@@ -112,3 +110,53 @@
         (syntax-local-implicit-equals-transformer-part-maybe #'() 4))
       (quote-implicit-equals-transformer-part 5)))
   "Test that using `local-implicit-equals-transformer-part` is similar to using `syntax-local-implicit-equals-transformer-part-maybe`")
+
+(define-simple-macro
+  (test-multiple-scopes four:expr five:id body:expr)
+  (let-implicit-equals-transformer-bindings ([four "cor"] [5 "rect"])
+    (let-syntax
+      (
+        [
+          five
+          (syntax-id-rules ()
+            [_ (quote-implicit-equals-transformer-part 5)])])
+      body)))
+
+(check-equal?
+  (test-multiple-scopes 4 five
+    (string-append (quote-implicit-equals-transformer-part 4) five))
+  "correct"
+  "Test that a `let-implicit-equals-transformer-bindings` can have two bindings which each have a different set of scopes")
+
+(check-equal?
+  (test-multiple-scopes 5 five
+    (string-append (quote-implicit-equals-transformer-part 5) five))
+  "correct"
+  "Test that a `let-implicit-equals-transformer-bindings` involving multiple sets of scopes actually lets us use the scope sets to distinguish the bindings")
+
+(define-simple-macro
+  (test-multiple-scopes-with-multiple-variables
+    four:expr five:id six:expr seven:id body:expr)
+  (let-implicit-equals-transformer-bindings
+    ([four "co"] [5 "rr"] [six "ec"] [7 "t"])
+    (let-syntax
+      (
+        [
+          five
+          (syntax-id-rules ()
+            [_ (quote-implicit-equals-transformer-part 5)])]
+        [
+          seven
+          (syntax-id-rules ()
+            [_ (quote-implicit-equals-transformer-part 7)])])
+      body)))
+
+(check-equal?
+  (test-multiple-scopes-with-multiple-variables 4 five 6 seven
+    (string-append
+      (quote-implicit-equals-transformer-part 4)
+      five
+      (quote-implicit-equals-transformer-part 6)
+      seven))
+  "correct"
+  "Test that a `let-implicit-equals-transformer-bindings` can have two pairs of bindings, each pair of which shares a different set of scopes")
