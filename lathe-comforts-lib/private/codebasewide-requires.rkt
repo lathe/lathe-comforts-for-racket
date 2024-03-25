ec19a20e93febbf6808362ef9c79d0d39b928e8d
Rocketnia <rokitna@hotmail.com>
2024-03-24 21:02:00 -0700

    In codebasewide-requires.rkt, update copyright dates.

--- lathe-comforts-lib/private/codebasewide-requires.rkt
+++ lathe-comforts-lib/private/codebasewide-requires.rkt
@@ -4,7 +4,7 @@
 ;
 ; An import list that's useful primarily for this codebase.
 
-;   Copyright 2022 The Lathe Authors
+;   Copyright 2022, 2024 The Lathe Authors
 ;
 ;   Licensed under the Apache License, Version 2.0 (the "License");
 ;   you may not use this file except in compliance with the License.
